94434040e2122d17392e68fbb02c5a2619edbbd1
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-09-01 11:43:02 +0430

    working with dynamic classes and computed property

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const app = Vue.createApp({
             buttonInner:"Show Content",
             x:0,
             y:0,
+            books: [
+                {author:"Stave Toltz", title:"The fraction of whole", img:"assets/1.jpg", isFav:true},
+                {author:"Matt Heig", title:"the midnight library", img:"assets/2.jpg", isFav:true},
+                {author:"George Orwell", title:"1984", img:"assets/nineteen-eighty-four-1984-george.jpg", isFav:false},
+            ]
         }
     },
 
@@ -34,17 +39,28 @@ const app = Vue.createApp({
         // }
 
 
-        eventHandler(e, number) {
-            console.log(e, e.type);
-            if(number){
-                console.log('====================================');
-                console.log(number);
-                console.log('====================================');
-            }
-        },
-        mouseMoveHandler(e){
-            this.x = e.offsetX;
-            this.y = e.offsetY;
+        // eventHandler(e, number) {
+        //     console.log(e, e.type);
+        //     if(number){
+        //         console.log('====================================');
+        //         console.log(number);
+        //         console.log('====================================');
+        //     }
+        // },
+        // mouseMoveHandler(e){
+        //     this.x = e.offsetX;
+        //     this.y = e.offsetY;
+        // }
+
+        toggleClass(book){
+            console.log(book.isFav);
+            book.isFav = !book.isFav;
+        }
+    },
+
+    computed: {
+        filteredBooks(){
+            return this.books.filter(b => b.isFav);
         }
     }
 });
