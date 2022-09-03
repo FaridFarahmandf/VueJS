fa3b078ad1506bfb4be8cda9136b1c614e410862
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-09-03 12:02:14 +0430

    creating a vue with CLI

---
+++ firstvueproject/src/App.vue
@@ -0,0 +1,46 @@
+<template> 
+  <div>
+    <h1>{{title}}</h1>
+    <Modal/>
+  </div>
+</template>
+
+<script>
+
+import Modal from "./components/Modal.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Modal,
+},
+  data() {
+    return {
+      title:"Hello Vue Cli App Component",
+    }
+  },
+
+  methods: {
+    clickHandler() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color:rgb(72, 126, 241)
+}
+</style>
---
+++ firstvueproject/src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="modal-holder">
+        <div class="modal">
+           <p>
+               My Modal Content...
+           </p> 
+        </div>
+    </div>
+</template>
+
+<style>
+    .modal-holder {
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .modal {
+        width:400px;
+        padding: 10px;
+        font-size: 15pt;
+        background: white;
+        border-radius:8px;
+    }
+</style>
